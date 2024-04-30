4818b4f63051e1934953951995003f99b34250b5
Jirayu Kaewprateep <jkaewprateep@gmail.com>
2024-04-29 21:26:09 -0400

    update

--- grihf-frontend_capstone_starter_code-master/src/App.js
+++ grihf-frontend_capstone_starter_code-master/src/App.js
@@ -8,6 +8,7 @@ import Sign_Up from "./Components/Sign_Up/Sign_Up";
 import Login from "./Components/Login/Login";
 import Instant_Consultation from "./Components/Instant_Consultation/InstantConsultation";
 import Notification from "./Components/Notification/Notification";
+import Feedback from "./Components/ReviewForm/ReviewForm";
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
                 <Route path="/signup" element={<Sign_Up/>}/>
                 <Route path="/instant-consultation" element={<Instant_Consultation/>}/>
                 <Route path="/notification" element={<Notification/>}/>
+                <Route path="/feedback" element={<Feedback/>}/>
               </Routes>
             
         </BrowserRouter>
